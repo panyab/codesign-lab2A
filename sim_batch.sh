36e158ad2eea7c3833763144060e87723b4c1d82
panya <pgbhinder@gmail.com>
2025-02-23 01:26:43 -0500

    sbatch script

---
+++ sim_batch.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH -Lab2A                           # Job name
+#SBATCH --ntasks=1
+#SBATCH -N30                             # Number of nodes and cores per node required
+#SBATCH --mem-per-cpu=4G                 # Memory per core
+#SBATCH -t12:00:00                       # Duration of the job
+#SBATCH -oReport-%j.out                  # Combined output and error messages file
+#SBATCH --mail-type=BEGIN,END,FAIL       # Mail preferences
+#SBATCH --mail-user=pbhinder3@gatech.edu # E-mail address for notifications
+cd $SLURM_SUBMIT_DIR                     # Change to working directory
+
+module load anaconda3                    # Load module dependencies
+conda activate hml_lab2
+
+echo "Submitting SLURM job..."
+bash run_sim.sh
+echo "SLURM job submitted!"
