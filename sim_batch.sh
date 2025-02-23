43cc6a2227a91855c35bbb32b2c60c386622480b
panya <pgbhinder@gmail.com>
2025-02-23 01:54:52 -0500

    fix sbatch tweaks

--- sim_batch.sh
+++ sim_batch.sh
@@ -3,9 +3,7 @@
 #SBATCH -Lab2A                          # Job name
 #SBATCH --ntasks=1
 #SBATCH -N1 --ntasks-per-node=50         # Number of nodes and cores per node required
-#SBATCH --mem-per-cpu=4G                 # Memory per core
-#SBATCH -t12:00:00                       # Duration of the job
-#SBATCH -oReport-%j.out                  # Combined output and error messages file
+#SBATCH -t8:00:00                       # Duration of the job
 #SBATCH --mail-type=BEGIN,END,FAIL       # Mail preferences
 #SBATCH --mail-user=pbhinder3@gatech.edu # E-mail address for notifications
 
