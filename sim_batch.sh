3068bb651011f3ab8c8d04c83bfa346a96337db4
panya <pgbhinder@gmail.com>
2025-02-23 01:48:17 -0500

    fix sbatch again

--- sim_batch.sh
+++ sim_batch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH -Lab2A                           # Job name
+#SBATCH -Lab2A                          # Job name
 #SBATCH --ntasks=1
 #SBATCH -N1 --ntasks-per-node=50         # Number of nodes and cores per node required
 #SBATCH --mem-per-cpu=4G                 # Memory per core
@@ -8,7 +8,7 @@
 #SBATCH -oReport-%j.out                  # Combined output and error messages file
 #SBATCH --mail-type=BEGIN,END,FAIL       # Mail preferences
 #SBATCH --mail-user=pbhinder3@gatech.edu # E-mail address for notifications
-cd $SLURM_SUBMIT_DIR                     # Change to working directory
+# cd $SLURM_SUBMIT_DIR                     # Change to working directory
 
 module load anaconda3                    # Load module dependencies
 conda activate hml_lab2
