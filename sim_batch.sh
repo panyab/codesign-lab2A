1ffced44fdc3a6e22c01d853c7c36dddb0594863
panya <pgbhinder@gmail.com>
2025-02-23 01:37:22 -0500

    fix sbatch

--- sim_batch.sh
+++ sim_batch.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -Lab2A                           # Job name
 #SBATCH --ntasks=1
-#SBATCH -N30                             # Number of nodes and cores per node required
+#SBATCH -N1 --ntasks-per-node=50         # Number of nodes and cores per node required
 #SBATCH --mem-per-cpu=4G                 # Memory per core
 #SBATCH -t12:00:00                       # Duration of the job
 #SBATCH -oReport-%j.out                  # Combined output and error messages file
